65edf7874edfc77909db5e7bf33bccf61090f7e6
Kevin S. Clarke <ksclarke@ksclarke.io>
2022-01-20 18:29:42 -0500

    [IIIF-1242] Minor tightening/clarifying edits

--- src/main/java/edu/ucla/library/iiif/auth/handlers/AccessModeHandler.java
+++ src/main/java/edu/ucla/library/iiif/auth/handlers/AccessModeHandler.java
@@ -104,7 +104,11 @@ public class AccessModeHandler implements Handler<RoutingContext> {
     }
 
     /**
-     * FIXME
+     * Expected access mode values: OPEN, TIERED, or ALL_OR_NOTHING. These determine the level of access a requested
+     * item is allowed.
+     *
+     * @see <a href="https://iiif.io/api/auth/1.0/#interaction-with-access-controlled-resources">Interaction with
+     *      Access-Controlled Resources </a>
      */
     public enum AccessMode {
         OPEN, TIERED, ALL_OR_NOTHING;
