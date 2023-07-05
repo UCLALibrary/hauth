f91cc9e9407a40f57d83c485bb3f41799ea537c5
Mark A. Matney, Jr <mmatney@library.ucla.edu>
2023-07-05 14:33:18 -0700

    Output visibility to access mode mapping in help doc

--- src/main/scripts/import-items/import-items.py
+++ src/main/scripts/import-items/import-items.py
@@ -9,6 +9,13 @@ from os.path import basename
 from requests import post
 from typing import List, Dict
 
+def docstring_parameter(*sub):
+    """Allows for formatting a method docstring with parameters. C.f. https://stackoverflow.com/a/10308363"""
+    def dec(obj):
+        obj.__doc__ = obj.__doc__.format(*sub)
+        return obj
+    return dec
+
 class AccessMode(Enum):
 
     """The access mode choices."""
@@ -16,17 +23,43 @@ class AccessMode(Enum):
     TIERED = 1
     ALL_OR_NOTHING = 2
 
+"""The mapping from visibility to access mode."""
+visibility_map = {
+    'open': AccessMode.OPEN,
+    'ucla': AccessMode.TIERED,
+    'private': AccessMode.TIERED, # will eventually mean UCLA all-or-nothing access; treat as tiered for now
+    'sinai': AccessMode.ALL_OR_NOTHING
+}
+
+def visibility_map_as_table() -> str:
+    """Renders a nice textual representation of the mapping from visibility to access mode."""
+    table = 'Visibility\tAccess mode\n'
+    table += '----------\t-----------\n'
+
+    for visibility, access_mode in visibility_map.items():
+        table += visibility + '\t\t' + str(access_mode.name) + '\n'
+
+    return table
+
 @click.command()
-@click.option('--access-mode', '-m', help='The access mode to use for all items in each INPUT_CSV.', required=False,
+@click.option('--access-mode', '-m', help='The access mode to use for all items in each INPUT_CSV (instead of the Visibility field value).', required=False,
               type=click.Choice([name for name, member in AccessMode.__members__.items()], case_sensitive=False))
 @click.option('--api-key', '-k', help='The API key for accessing the admin API.', required=True)
 @click.argument('hauth-base-url', nargs=1)
 @click.argument('input-csv', required=True, nargs=-1, type=click.File('r'))
+@docstring_parameter(visibility_map_as_table())
 def import_items(access_mode, api_key, hauth_base_url, input_csv):
-    """Import items into Hauth.
+    """
+Import items into Hauth.
 
-    Instructs the Hauth instance at HAUTH_BASE_URL to use the specified access mode for all items in each INPUT_CSV.
-    Exits with zero status only if all items are imported successfully.
+Instructs the Hauth instance at HAUTH_BASE_URL to register the access modes of all items in each INPUT_CSV.
+
+Unless --access-mode is supplied, the access mode of each item is determined from the Visibility field of its corresponding INPUT_CSV as follows:
+
+\b
+{0}
+
+Exits with zero status only if all items are imported successfully.
     """
     request_url = hauth_base_url + '/items'
     request_headers = {'Content-Type': 'application/json', 'X-API-KEY': api_key}
@@ -68,15 +101,8 @@ def get_access_mode(row: Dict) -> AccessMode:
     field_name = 'Visibility'
     field_value = row[field_name]
 
-    if field_value == 'open':
-        return AccessMode.OPEN
-    elif field_value == 'ucla':
-        return AccessMode.TIERED
-    elif field_value == 'private':
-        # UCLA all-or-nothing access, which is not yet implemented; treat as UCLA tiered access for now
-        return AccessMode.TIERED
-    elif field_value == 'sinai':
-        return AccessMode.ALL_OR_NOTHING
+    if field_value in visibility_map:
+        return visibility_map[field_value]
     else:
         raise ValueError('Unknown {} "{}" for row "{}"'.format(field_name, field_value, row))
 
